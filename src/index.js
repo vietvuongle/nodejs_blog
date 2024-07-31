9c76a734f998fbb22bb113330979ad3495fe2857
vietvuongle <vietvuongf@gmail.com>
2024-07-31 10:36:59 +0700

    init commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const morgan = require('morgan')
-const path = require('path')
-const express = require('express')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const morgan = require('morgan');
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(morgan('combined'))
 
