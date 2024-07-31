e44088e7a4d5307ea8db4c8e4c47ba5bd6de3766
vietvuongle <vietvuongf@gmail.com>
2024-07-31 15:41:20 +0700

    init commit

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewsController {
+
+    //[GET] /news
+    index(req, res){
+        res.render('news')
+    }
+
+    show(req, res){
+        res.send('New detail')
+    }
+}
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 
 app.use(morgan('combined'))
 
@@ -13,13 +19,7 @@ app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, './resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
